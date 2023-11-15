12583a0299020362cefcd59c19c483ec3adb2261
Your Name <you@example.com>
2023-11-15 03:44:19 +0200

    finish the structure of UI

--- src/main/java/com/fantasy/fantasyleague/Group/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fantasy.fantasyleague.Group;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends
-        JpaRepository<GroupFantasy, Integer> {
-
-}
--- src/main/java/com/fantasy/fantasyleague/Registiration/Service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.fantasy.fantasyleague.Registiration.Service;
-
-import com.fantasy.fantasyleague.Registiration.DTO.SignInDTO;
-import com.fantasy.fantasyleague.Registiration.DTO.SignUpDTO;
-
-public interface AdminService {
-
-    String validateAdmin(SignInDTO signInDTO);
-}
