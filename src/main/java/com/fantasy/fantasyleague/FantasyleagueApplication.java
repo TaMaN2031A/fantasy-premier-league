97920e1da37c9e5153bc84748478f973260e59db
Abdullah Taman <bdlltmn@gmail.com>
2023-11-24 10:56:00 +0200

    test

--- src/main/java/com/fantasy/fantasyleague/FantasyleagueApplication.java
+++ src/main/java/com/fantasy/fantasyleague/FantasyleagueApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class FantasyleagueApplication {
 
 	public static void main(String[] args) {
-		int lol = 1;
+		int lol = 2;
 		SpringApplication.run(FantasyleagueApplication.class, args);
 	}
 
