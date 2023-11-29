32ee8b7feb5290233674e113e8d9adfc69697827
Ahmedkhalilelzainy <es-AhmedKhalil2025@alexu.edu.eg>
2023-11-29 03:17:55 +0200

    deleted some wrong classes

--- Backend/src/main/java/com/fantasy/fantasyleague/FaqRule/FAQRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fantasy.fantasyleague.FaqRule;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FAQRepository extends
-        JpaRepository<FAQ, Integer> {
-
-}
