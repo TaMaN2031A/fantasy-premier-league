5fab5d8b77e4dada50e5c962ee7da046d88d7445
Amr Ahmed <es-AmrA.Abdelazim2025@alexu.edu.eg>
2023-12-11 21:44:00 +0200

    Update home page before routes

--- Frontend/src/myStructure/Home/Home.js
+++ Frontend/src/myStructure/Home/Home.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import Card from "./Card";
+import Profile from "./Profile";
+import plLogo from "../Authentication/logo.png";
 
 function Home(props) {
-    return (
-        <div className="bg-gradient-to-r from-slate-800 to-gray-900">
-            <h1>Home</h1>
-        </div>
-    );
+  const items = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const list = items.map(() => (
+    <Card Title="Matches" details="HAve fun and play with friends" />
+  ));
+
+  return (
+    <div className="bg-gradient-to-r from-slate-800 to-gray-900 min-h-screen">
+      <img className="object-scale-down mx-auto shadow-md hover:shadow-lg transition duration-300 ease-in-out"  src={plLogo} alt="logo" />
+      <div className=" justify-center text-center">
+        <Profile />
+        <hr className="border-b-2 border-gray-500 w-3/4 mx-auto" />
+        <div className="flex flex-wrap justify-center">{list}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- Frontend/src/myStructure/collection.js
+++ Frontend/src/myStructure/collection.js
@@ -1,6 +1,6 @@
 export const adminPrv = "ADMIN";
 export const userPrv = "USER";
-export const hostOfBack = "http://localhost:8080";
+export const hostOfBack = "http://localhost:8081";
 export const clientID = "157567928601-tpi54p30l3rpsdlmrh7on2fmfup3tpct.apps.googleusercontent.com";
 
 export const paths = {
