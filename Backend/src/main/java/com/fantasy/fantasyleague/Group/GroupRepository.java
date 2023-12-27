cf387ac530df6dc9056baeab673250187a665f79
ahmed elzainy <94832121+Ahmedkhalilelzainy@users.noreply.github.com>
2023-12-28 00:37:15 +0200

    Revert "Fpl 69 fantasy database"

---
+++ Backend/src/main/java/com/fantasy/fantasyleague/Group/GroupRepository.java
@@ -0,0 +1,11 @@
+package com.fantasy.fantasyleague.Group;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends
+        JpaRepository<GroupFantasy, Integer> {
+
+}
--- Backend/src/main/java/com/fantasy/fantasyleague/fantasyGame/Model/FormationStatusHistory/FormationStatusHistoryComposite.java
+++
@@ -1,8 +0,0 @@
-package com.fantasy.fantasyleague.fantasyGame.Model.FormationStatusHistory;
-
-import java.io.Serializable;
-
-public class FormationStatusHistoryComposite implements Serializable {
-    private String user;
-    private int week_no;
-}
