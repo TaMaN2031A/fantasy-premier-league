23dabf787449821b45004c1045743739b462c646
mohamedarous1 <mohamed.arous940@gmail.com>
2023-12-28 15:34:29 +0200

    debugg the group

--- Backend/src/main/java/com/fantasy/fantasyleague/Registiration/Repository/UserRepository.java
+++ Backend/src/main/java/com/fantasy/fantasyleague/Registiration/Repository/UserRepository.java
@@ -1,4 +1,5 @@
 package com.fantasy.fantasyleague.Registiration.Repository;
+
 import com.fantasy.fantasyleague.Registiration.Model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -6,6 +7,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
@@ -15,9 +17,13 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email OR u.userName = :userName")
     User findByEmailOrUserName(String email, String userName);
-// get all users
+
+    // get all users
     @Query("SELECT new com.fantasy.fantasyleague.Registiration.DTO.AdminPromotionDTO(u.email, u.userName) FROM User u")
     Page<User> findAllUsers(Pageable pageable);
-// search by specification
+
+    // search by specification
     Page<User> findAll(Specification<User> spec, Pageable pageable);
+
+    User findByUserName(String userName);
 }
